b74d33f18a12af92eda4f9f94b5dc150977fb1b5
Bennett, Ryan <rbennett@safenetconsulting.com>
2018-01-05 10:24:15 -0600

    Mockup

---
+++ WebApplication3/gulpfile.js
@@ -0,0 +1,34 @@
+﻿/// <binding BeforeBuild='default' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+// Dependency Dirs
+var deps = {
+    "jquery": {
+        "dist/*": ""
+    },
+    "bootstrap": {
+        "dist/**/*": ""
+    }
+    // ...
+
+};
+
+
+gulp.task('default', function () {
+    var streams = [];
+
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/vendor/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+});
